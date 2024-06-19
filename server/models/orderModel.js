dac863e3d296213ebe1f3c39e0f62e7756e013c8
Tanaka Chidemo <chidemo@icloud.com>
2024-06-19 22:30:50 +0400

    Added server code and updated models

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -1,37 +1,39 @@
-orderModel
-const mongoose = require('mongoose');
+orderModel;
+const mongoose = require("mongoose");
 const orderSchema = new mongoose.Schema({
-admin: [{type: mongoose.Schema.ObjectId, ref: 'User'}],
-deliveryDate: {
-type: Date,
-required: [true, 'An order must have a delivery date.'],
-},
-closingDate: {
-type: Date,
-required: [true, 'An order must have a closing date.'],
-},
-closingTime: {
-type: Time,
-required: [true, 'An order must have a closing time.'],
-},
-paymentDeadline: {
-type: Date,
-required: [true, 'An order must have a payment deadline.'],
-},
-paymentDeadlineTime: {
-type: Time,
-required: [true, 'An order must have a closing time.'],
-},
-paymentDetails: {
-type: String,
-},
+  admin: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+  deliveryDate: {
+    type: Date,
+    required: [true, "An order must have an estimated delivery date."],
+  },
+  closingDate: {
+    type: Date,
+    required: [true, "An order must have a closing date."],
+  },
+  closingTime: {
+    type: Time,
+    required: [true, "An order must have a closing time."],
+  },
+  paymentDeadline: {
+    type: Date,
+    required: [true, "An order must have a payment deadline."],
+  },
+  paymentDeadlineTime: {
+    type: Time,
+    required: [true, "An order must have a closing time."],
+  },
+  paymentDetails1: {
+    type: String,
+  },
+  paymentDetails2: {
+    type: String,
+  },
 });
 
 orderSchema.pre(/^find/, function (next) {
-this.populate({
-path: 'admin',
-select: 'username',
+  this.populate({
+    path: "admin",
+    select: "username",
+  });
+  next();
 });
-next();
-});
-
